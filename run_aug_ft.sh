69acf554ab9d7276496a5e5f54fb6855c31ece14
Dan Iter <daniter@stanford.edu>
2022-03-23 04:00:13 +0000

    runners for nn and kmeans exp

--- run_aug_ft.sh
+++ run_aug_ft.sh
@@ -8,12 +8,30 @@ VOCAB_PATH="../../bucket/cns/ym-d/home/daniter/flax_wmt/t2t/sentencepiece_model"
 IS_SCORE_PATH="../../bucket/cns/pw-d/home/daniter/ende/clf_src/ende_src_bert_scores.csv"
 NUM_TO_KEEP=6000
 SAMPLE_SIZE=-1
+DATASET_NAME="newscommentary_paracrawl"
+
+DATA_DIR_SELECTED="../../bucket/selected_data/training_data.csv"
+IS_SCORES_PATH_SELECTED_KMEANS="../../bucket/selected_data/nn_ranking_scores.csv"
+IS_SCORES_PATH_SELECTED_NN="../../bucket/selected_data/kmeans_ranking_scores.csv"
+DATASET_NAME_SELECTED="selected_data"
 
 BATCHSZ=128
 while [[ $# -gt 0 ]]; do
   case $1 in
     --gpu)
       BATCHSZ=32
+      shift # past argument
+      ;;
+    --selected)
+      DATASET_NAME=$DATASET_NAME_SELECTED
+      DATA_DIR=$DATA_DIR_SELECTED
+      IS_SCORES_PATH=$IS_SCORES_PATH_SELECTED_NN
+      shift # past argument
+      ;;
+    --kmeans)
+      DATASET_NAME=$DATASET_NAME_SELECTED
+      DATA_DIR=$DATA_DIR_SELECTED
+      IS_SCORES_PATH=$IS_SCORES_PATH_SELECTED_KMEANS
       shift # past argument
       ;;
     --num_to_keep)
@@ -41,7 +59,7 @@ done
 echo "Using batch size $BATCHSZ"
 
 python3 train_aug_ft.py --model_dir=$FT_DIR --ft_dataset_name='newscommentary_ft' \
-  --dataset_name='newscommentary_paracrawl' \
+  --dataset_name=$DATASET_NAME \
   --batch_size=$BATCHSZ --num_train_steps=4000 --restore_checkpoints \
   --emb_dim=512 --mlp_dim=2048 --num_heads=8 \
   --vocab_path=$VOCAB_PATH --paracrawl_size=4500000 \
--- run_sample_exp.sh
+++ run_sample_exp.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
 
-bash run_aug_ft.sh --num_to_keep 6000 --output_dir ../results/aug_ft_keep6k
-bash run_aug_ft.sh --num_to_keep 12000 --output_dir ../results/aug_ft_keep12k
-bash run_aug_ft.sh --num_to_keep 24000 --output_dir ../results/aug_ft_keep24k
-bash run_aug_ft.sh --num_to_keep 50000 --output_dir ../results/aug_ft_keep50k
-bash run_aug_ft.sh --num_to_keep 100000 --output_dir ../results/aug_ft_keep100k
+# bash run_aug_ft.sh --num_to_keep 6000 --output_dir ../results/aug_ft_keep6k
+# bash run_aug_ft.sh --num_to_keep 12000 --output_dir ../results/aug_ft_keep12k
+# bash run_aug_ft.sh --num_to_keep 24000 --output_dir ../results/aug_ft_keep24k
+# bash run_aug_ft.sh --num_to_keep 50000 --output_dir ../results/aug_ft_keep50k
+# bash run_aug_ft.sh --num_to_keep 100000 --output_dir ../results/aug_ft_keep100k
 
-## downsampling
+# ## downsampling
 
-# 12k
-bash run_aug_ft.sh --num_to_keep 12000 --sample_size 6000 \
---output_dir ../results/aug_ft_keep12k_samp6k
+# # 12k
+# bash run_aug_ft.sh --num_to_keep 12000 --sample_size 6000 \
+# --output_dir ../results/aug_ft_keep12k_samp6k
 
-# 24k
-bash run_aug_ft.sh --num_to_keep 24000 --sample_size 6000 \
---output_dir ../results/aug_ft_keep24k_samp6k
-bash run_aug_ft.sh --num_to_keep 24000 --sample_size 12000 \
---output_dir ../results/aug_ft_keep24k_samp12k
+# # 24k
+# bash run_aug_ft.sh --num_to_keep 24000 --sample_size 6000 \
+# --output_dir ../results/aug_ft_keep24k_samp6k
+# bash run_aug_ft.sh --num_to_keep 24000 --sample_size 12000 \
+# --output_dir ../results/aug_ft_keep24k_samp12k
 
-# 50k
-bash run_aug_ft.sh --num_to_keep 50000 --sample_size 6000 \
---output_dir ../results/aug_ft_keep50k_samp6k
-bash run_aug_ft.sh --num_to_keep 50000 --sample_size 12000 \
---output_dir ../results/aug_ft_keep50k_samp12k
-bash run_aug_ft.sh --num_to_keep 50000 --sample_size 24000 \
---output_dir ../results/aug_ft_keep50k_samp24k
+# # 50k
+# bash run_aug_ft.sh --num_to_keep 50000 --sample_size 6000 \
+# --output_dir ../results/aug_ft_keep50k_samp6k
+# bash run_aug_ft.sh --num_to_keep 50000 --sample_size 12000 \
+# --output_dir ../results/aug_ft_keep50k_samp12k
+# bash run_aug_ft.sh --num_to_keep 50000 --sample_size 24000 \
+# --output_dir ../results/aug_ft_keep50k_samp24k
 
-# 100k
-bash run_aug_ft.sh --num_to_keep 100000 --sample_size 6000 \
---output_dir ../results/aug_ft_keep100k_samp6k
-bash run_aug_ft.sh --num_to_keep 100000 --sample_size 12000 \
---output_dir ../results/aug_ft_keep100k_samp12k
-bash run_aug_ft.sh --num_to_keep 100000 --sample_size 24000 \
---output_dir ../results/aug_ft_keep100k_samp24k
-bash run_aug_ft.sh --num_to_keep 100000 --sample_size 50000 \
---output_dir ../results/aug_ft_keep100k_samp50k
+# # 100k
+# bash run_aug_ft.sh --num_to_keep 100000 --sample_size 6000 \
+# --output_dir ../results/aug_ft_keep100k_samp6k
+# bash run_aug_ft.sh --num_to_keep 100000 --sample_size 12000 \
+# --output_dir ../results/aug_ft_keep100k_samp12k
+# bash run_aug_ft.sh --num_to_keep 100000 --sample_size 24000 \
+# --output_dir ../results/aug_ft_keep100k_samp24k
+# bash run_aug_ft.sh --num_to_keep 100000 --sample_size 50000 \
+# --output_dir ../results/aug_ft_keep100k_samp50k
+
+# NN
+bash run_aug_ft.sh --num_to_keep 6000 --output_dir ../results/aug_ft_nn_keep6k --selected
+bash run_aug_ft.sh --num_to_keep 12000 --output_dir ../results/aug_ft_nn_keep12k --selected
+bash run_aug_ft.sh --num_to_keep 24000 --output_dir ../results/aug_ft_nn_keep24k --selected
+bash run_aug_ft.sh --num_to_keep 50000 --output_dir ../results/aug_ft_nn_keep50k --selected
+bash run_aug_ft.sh --num_to_keep 100000 --output_dir ../results/aug_ft_nn_keep100k --selected
+
+# KMEANS
+bash run_aug_ft.sh --num_to_keep 6000 --output_dir ../results/aug_ft_kmeans_keep6k --kmeans
+bash run_aug_ft.sh --num_to_keep 12000 --output_dir ../results/aug_ft_kmeans_keep12k --kmeans
+bash run_aug_ft.sh --num_to_keep 24000 --output_dir ../results/aug_ft_kmeans_keep24k --kmeans
+bash run_aug_ft.sh --num_to_keep 50000 --output_dir ../results/aug_ft_kmeans_keep50k --kmeans
+bash run_aug_ft.sh --num_to_keep 100000 --output_dir ../results/aug_ft_kmeans_keep100k --kmeans
